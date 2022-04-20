4d32e949d2dc6722a256f7d4b62fadf490b33c46
Atharva Joshi <atharvajoshi2603@gmail.com>
2022-04-21 04:49:52 +0530

    outer pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,39 @@
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import "./App.css";
+import backgroundImage from "./components/images/background.jpg";
 
+import UsersList from "./components/users/UsersList";
 import Home from "./components/common/Home";
 import Register from "./components/common/Register";
-import SignIn from "./components/common/signin";
-import Therapist_home from "./components/Therapist/t_home";
-import Parent_home from "./components/Parent/p_home";
-import Activities_page from "./components/Therapist/t_activity";
+import Sidebar from "./components/templates/Sidebar";
+import Login from "./components/common/Login";
+import Profile from "./components/users/Profile";
+
 const Layout = () => {
-  return (
-    <div>
-      <div className="container">
+  if (localStorage.getItem("id")) {
+    return (
+      <div>
         <Outlet />
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      // <div></div>
+      <Outlet />
+    );
+  }
 };
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-         <Route path="/" element={<Layout />}> 
+        <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="signin" element={<SignIn />} />
+          <Route path="users" element={<UsersList />} />
           <Route path="register" element={<Register />} />
-          <Route path="t_home" element={<Therapist_home />} />
-          <Route path="p_home" element={<Parent_home />} />
-          <Route path="t_activity" element={<Activities_page />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
